3be2b97a8328a57cac162f85598db90a3e44bc85
FrenchMontana10 <cecilofori1@gmail.com>
2022-06-22 23:04:03 +0000

    fifth commit

---
+++ 0x04/assignment.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define TOTAL_SIZE 20
+
+int main(void)
+{
+	int data[TOTAL_SIZE];
+
+	int total = 0, i, j, product;
+
+	float avg;
+
+	for(i = 0; i < TOTAL_SIZE; i++);
+	{
+		product = i * TOTAL_SIZE;
+
+		data[i] = product;
+
+		total = total + product;
+	}
+
+	avg = (total / TOTAL_SIZE);
+
+	printf("Average is %f\n", avg);
+
+	for(j = TOTAL_SIZE; j > 0; j--)
+	{
+		printf("The element at %d", j);
+
+		printf("is %d\n", data[j]);
+	}
+
+	return 0;
+}
