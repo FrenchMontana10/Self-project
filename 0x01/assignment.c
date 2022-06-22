4029d3f4902089b23a4f798ce9336c4fb661fa54
FrenchMontana10 <cecilofori1@gmail.com>
2022-06-22 22:25:10 +0000

    first commit

---
+++ 0x01/assignment.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int numbers[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+	printf("element at %d is %d\n", 0, numbers[0]);
+
+	printf("element at %d is %d\n", 1, numbers[1]);
+
+	printf("element at %d is %d\n", 2, numbers[2]);
+
+	printf("element at %d is %d\n", 3, numbers[3]);
+
+	printf("element at %d is %d\n", 4, numbers[4]);
+
+	printf("element at %d is %d\n", 5, numbers[5]);
+
+	printf("element at %d is %d\n", 6, numbers[6]);
+
+	printf("element at %d is %d\n", 7, numbers[7]);
+
+	printf("element at %d is %d\n", 8, numbers[8]);
+
+	printf("element at %d is %d\n", 9, numbers[9]);
+
+	return 0;
+}
