c05537d4d249efa32325d417807f367b631f35e3
FrenchMontana10 <cecilofori1@gmail.com>
2022-06-22 22:50:53 +0000

    fourth commit

---
+++ 0x03/assignment.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	const unsigned int SIZE = 20;
+
+	int n[SIZE];
+
+	int position, index;
+
+	for(position = 0; position < SIZE; position++)
+	{
+		n[position] = position + 5;
+	}
+
+	for(index = 0; index < SIZE; index++)
+	{
+		printf("The Element at index %d is ", index);
+
+		printf("%d\n", n[index]);
+	}
+
+	printf("Program End\n");
+
+	return 0;
+}
