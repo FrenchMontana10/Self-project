7f04367336b7ebcaf55e0b5c8d6b1bee454fcaa2
FrenchMontana10 <cecilofori1@gmail.com>
2022-06-23 01:55:55 +0000

    something

--- interpretation1.c
+++ interpretation1.c
@@ -1,8 +1,9 @@
 INTERPRETATION OF QUESTION 3.
-An variable called SIZE of type const unsigned int was declared and initialized to 20.
+A variable called SIZE of type const unsigned int was declared and initialized to 20.
 An array of type integer n[SIZE] was also declared in the next line of code.
 Integer variables namely position and index were also declared.
-We started a for loop and initialized position to zero. We gave a condition that position should be less than anything that the variable SIZE contains which is 20. And we said that position should always update itself by 1 everytime condition our condition is true.
+We started a for loop and initialized position to zero. We gave a condition that position should be less than anything that the variable SIZE contains which is 20. And we said that position should always update itself 
+by 1 everytime condition our condition is true.
 So in the code block of the for loop, we said declared and initialized the array n[position] to position + 4.
 We then broke out of our for loop.
 We started another for loop and initialized index to 0. We gave a condition that index should be less than anything that the variable SIZE contains which is 20.
